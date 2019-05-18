6bbd76d186a45bf7d447e314608d8b39e4f04fb0
piotrkazm <kazioks@gmail.com>
2019-05-18 15:51:29 +0200

    Create enum Nominal

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,35 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
+        for (String arg : args) {
+            try {
+                int amount = Integer.parseInt(arg);
+                System.out.println("Giving change for" + arg);
+                printExchange(amount);
+            }catch (RuntimeException e) {
+                System.out.println("Something went wrong!");
+            }
+        }
 
 
-       for(Nominal value : Nominal.values()) {
-        System.out.println(value);
+
     }
-}
+
+    public static void printExchange(int amont) {
+        if (amont % 10 !=0) {
+            throw new RuntimeException();
+        }
+        Nominal[] values = Nominal.values();
+        int pointer = 0;
+        while (amont > 0) {
+            if (values[pointer].getValue() <= amont) {
+                System.out.println(values[pointer]);
+                amont -= values[pointer].getValue();
+            } else {
+                pointer++;
+            }
+
+
+        }
+    }
 }
