833ce341c7d6251ab159e3fc21df63704e91d530
piotrkazm <kazioks@gmail.com>
2019-05-18 15:11:28 +0200

    Create enum Nominal

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+       for(Nominal value : Nominal.values()) {
+        System.out.println(value);
+    }
+}
+}
